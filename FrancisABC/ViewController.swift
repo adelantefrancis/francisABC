a11ab34e71ebe297aad1a738deabfa2aaad7810b
francis adelante <adelante.francis@gmail.com>
2019-09-04 19:29:38 +0800

    set up project

---
+++ FrancisABC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrancisABC
+//
+//  Created by Francis Adelante on 9/4/19.
+//  Copyright © 2019 Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
